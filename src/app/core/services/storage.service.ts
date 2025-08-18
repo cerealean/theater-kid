7da3590839d8a476afd096404a4fe78576693b61
Michael Crawford <cerealean@users.noreply.github.com>
2025-08-18 02:43:34 -0500

    feat: add storage service

---
+++ src/app/core/services/storage.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+
+type CanBeStored = string | number | boolean | object | undefined | null;
+
+@Injectable({
+  providedIn: 'root',
+})
+export class StorageService {
+  setItem<T = CanBeStored>(key: string, value: T): void {
+    const serialized = JSON.stringify(value);
+    localStorage.setItem(key, serialized);
+  }
+
+  getItem<T = CanBeStored>(key: string): T {
+    const item = localStorage.getItem(key);
+    return item ? JSON.parse(item) : (undefined as T);
+  }
+
+  removeItem(key: string): void {
+    localStorage.removeItem(key);
+  }
+}
