291ddbf0be1a15ad9253a375591445688365f8e1
Michael Crawford <cerealean@users.noreply.github.com>
2025-08-18 13:58:39 -0500

    chore: fix improper typing

--- src/app/core/services/storage.service.ts
+++ src/app/core/services/storage.service.ts
@@ -10,14 +10,15 @@ interface StorageItem {
   providedIn: 'root',
 })
 export class StorageService {
-  setItem<T = CanBeStored>(key: string, value: T): void {
+  setItem<T extends CanBeStored>(key: string, value: T): void {
     const serialized = JSON.stringify({ item: value, timestamp: Date.now() });
     localStorage.setItem(key, serialized);
   }
 
-  getItem(key: string): CanBeStored {
+  getItem<T extends CanBeStored>(key: string): T {
     const item = localStorage.getItem(key);
-    return this.safeParse<StorageItem>(item)?.item;
+
+    return this.safeParse<StorageItem>(item)?.item as T;
   }
 
   removeItem(key: string): void {
